13b335b54d8296192074ad093c0a5bee38b7d49e
BrianGorter078 <briang@145.24.235.5>
2015-10-06 15:29:32 +0200

    Server

--- DEV FX/src/dev/fx/ServerController.java
+++ DEV FX/src/dev/fx/ServerController.java
@@ -9,10 +9,15 @@ import java.net.URL;
 import java.util.Collection;
 import java.util.List;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -27,45 +32,72 @@ public class ServerController implements Initializable {
     private ListView serverList;
     @FXML
     private Label serverCountLabel;
+    @FXML
+    private TableView<serverTableEntry> serversTable;
+    @FXML
+    private TableColumn<serverTableEntry, String> adress; 
+    @FXML
+    private TableColumn<serverTableEntry, Integer> users; 
+    @FXML
+    private TableColumn<serverTableEntry, String> location; 
+    @FXML
+    private TableColumn<serverTableEntry, Integer> maxUsers; 
+    @FXML
+    private TableColumn<serverTableEntry, String> name;
     
+    private ObservableList<serverTableEntry> serverData;
     private int serverCount = 0;
     private String serverAdress;
     private int connectedUsers;
-    private String serverlocation;
+    private String serverLocation;
     private int serverMaxUsers;
     private String serverName;
     private Collection<Users> UsersCollection;
 
        @Override
     public void initialize(URL location, ResourceBundle resources) {
+            
         getServers();
+            
+            /*TableColumn address = new TableColumn("Address");
+            address.setCellFactory(new PropertyValueFactory<serverTableEntry, String>(""));*/
     }
     
     public void getServers()
     {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("DEV_FXPU");
         EntityManager em = emf.createEntityManager();        
-        List results = em.createNamedQuery("Servers.findByName")
-                .setParameter("name", "UK")
+        List results = em.createNamedQuery("Servers.findAll")
                 .getResultList();
         if(!results.isEmpty())
         {
+            serverData = FXCollections.observableArrayList();
            for (int i=0 ;i < results.size(); i++)
            {
+              Servers s = (Servers) results.get(i);
+              serverAdress = s.getAdress();
+              connectedUsers = s.getConnectedUsers();
+              serverLocation = s.getLocation();
+              serverMaxUsers = s.getMaxUsers();
+              serverName = s.getName();
+              
+              serverTableEntry se = new serverTableEntry(serverAdress, connectedUsers, serverLocation, serverMaxUsers, serverName);
+              serverData.add(se);
+               
+            }
+               
+                adress.setCellValueFactory(new PropertyValueFactory<dev.fx.serverTableEntry, String>("adress"));
+                users.setCellValueFactory(new PropertyValueFactory<dev.fx.serverTableEntry, Integer>("users"));
+                location.setCellValueFactory(new PropertyValueFactory<dev.fx.serverTableEntry, String>("location"));        
+                maxUsers.setCellValueFactory(new PropertyValueFactory<dev.fx.serverTableEntry, Integer>("maxUsers"));        
+                name.setCellValueFactory(new PropertyValueFactory<dev.fx.serverTableEntry, String>("name"));
+            
                 
-                Servers s = new Servers();
-                serverAdress = s.getAdress() ;
-                connectedUsers = s.getConnectedUsers();   
-                serverlocation = s.getLocation();
-                serverMaxUsers = s.getMaxUsers();
-                serverName = s.getName();
-                UsersCollection = s.getUsersCollection();
+                serversTable.setItems(serverData);
                 
                 setServers();
                 serverCount += 1;
            }
-            
-        }
         else
         {
             setServers();
